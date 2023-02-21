05c85c2224ed9cf056aa268560a96af19b87f916
Alan Horne <ahorne@retrostudios.com>
2023-02-20 19:50:39 -0600

    wip

--- CSharp8/Program.cs
+++ CSharp8/Program.cs
@@ -2,11 +2,47 @@
 
 namespace CSharp8
 {
+    // Readonly members
+    // Default interface methods
+    // Pattern matching enhancements:
+    //    Switch expressions
+    //    Property patterns
+    //    Tuple patterns
+    //    Positional patterns
+    // Using declarations
+    // Static local functions
+    // Disposable ref structs
+    // Nullable reference types
+    // Asynchronous streams
+    // Indices and ranges
+    // Null-coalescing assignment
+    // Unmanaged constructed types
+    // Stackalloc in nested expressions
+    // Enhancement of interpolated verbatim strings
+
+    public struct ExampleStruct
+    {
+        public int Field1;
+        public int Field2;
+
+        public readonly int Sum() => Field1 + Field2;
+
+        public ExampleStruct(int field1, int field2)
+        {
+            Field1 = field1;
+            Field2 = field2;
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var structInstance = new ExampleStruct(1, 2);
+
+            structInstance.Field1 = 3;
+
+            var sum = structInstance.Sum();
         }
     }
 }
--- CSharp9/Program.cs
+++ CSharp9/Program.cs
@@ -2,11 +2,50 @@
 
 namespace CSharp9
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+   // Records
+   // Init only setters
+   // Top-level statements
+   // Pattern matching enhancements
+   // Performance and interop
+   //    Native sized integers
+   //    Function pointers
+   //    Suppress emitting localsinit flag
+   // Fit and finish features
+   //    Target-typed new expressions
+   //    static anonymous functions
+   //    Target-typed conditional expressions
+   //    Covariant return types
+   //    Extension GetEnumerator support for foreach loops
+   //    Lambda discard parameters
+   //    Attributes on local functions
+   // Support for code generators
+   //    Module initializers
+   //    New features for partial methods
+   // Warning wave 5
+
+   public struct ExampleStruct
+   {
+      public int Field1;
+      public int Field2;
+
+      public readonly int Sum() => Field1 + Field2;
+
+      public ExampleStruct(int field1, int field2)
+      {
+         Field1 = field1;
+         Field2 = field2;
+      }
+   }
+    
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var structInstance = new ExampleStruct(1, 2);
+
+         structInstance.Field1 = 3;
+
+         var sum = structInstance.Sum();
+      }
+   }
 }
